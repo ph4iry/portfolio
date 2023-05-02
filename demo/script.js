d24fd2d248f9eb4fe0d605f44f511804f7a79c5b
Phaedra <71579580+ph4iry@users.noreply.github.com>
2023-05-02 17:31:55 -0400

    started rocket trail

--- demo/script.js
+++ demo/script.js
@@ -1,27 +1,3 @@
-// $(document).on('scroll', (event) => {
-//   console.log('hi');
-//   const pos = getVerticalScrollPercentage(document.body)
-//   document.body.innerHTML = "<span>" + Math.round(pos) + "%<span>"
-//   // const offset = this.offset;
-//   // console.log((offset.top + $(this).outerHeight(true)))
-// });
-
-const spaceTrail = document.querySelector('#path svg');
-spaceTrail.setAttribute('viewBox', `0 0 ${innerWidth} ${innerHeight}`);
-spaceTrail.setAttribute('width', `${innerWidth}`);
-spaceTrail.setAttribute('height', `${innerHeight}`);
-
-// $(window).on('scroll', function(){
-//   const s = $(window).scrollTop(),
-//         d = $(document).height(),
-//         c = $(window).height();
-
-//   var scrollPercent = (s / (d - c)) * 100;
-  
-//   console.clear();
-//   console.log(scrollPercent);
-// })
-
 const spaceship = document.querySelector("#spaceship")
 const path = document.querySelector("#path path")
 const paragraphs = document.querySelectorAll('p')
