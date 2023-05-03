d5987f0447e4617750b244ae54bf3b4fc28b0fd9
Phaedra <71579580+ph4iry@users.noreply.github.com>
2023-05-03 17:51:51 -0400

    redid project structure and added rocket path

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import * as svgpath from '../node_modules/svgpath';
+import $ from './lib/jquery.slim.min.js';
 // $(document).on('scroll', (event) => {
 //   console.log('hi');
 //   const pos = getVerticalScrollPercentage(document.body)
@@ -7,48 +9,42 @@
 // });
 
 const spaceTrail = document.querySelector('#path svg');
-spaceTrail.setAttribute('viewBox', `0 0 ${innerWidth} ${innerHeight}`);
-spaceTrail.setAttribute('width', `${innerWidth}`);
-spaceTrail.setAttribute('height', `${innerHeight}`);
-
-// $(window).on('scroll', function(){
-//   const s = $(window).scrollTop(),
-//         d = $(document).height(),
-//         c = $(window).height();
-
-//   var scrollPercent = (s / (d - c)) * 100;
-  
-//   console.clear();
-//   console.log(scrollPercent);
-// })
+// spaceTrail.setAttribute('viewBox', `0 0 ${innerWidth} ${document.body.scrollHeight}`);
+// spaceTrail.setAttribute('width', `${innerWidth}`);
+// spaceTrail.setAttribute('height', `${document.body.scrollHeight}`);
 
 const spaceship = document.querySelector("#spaceship")
-const path = document.querySelector("#path path")
+const path = document.querySelector("#path path")//
+const resizedPath = svgpath(path.getAttribute("d")).scale(1) // , document.body.scrollHeight / spaceTrail.clientHeight).toString();
+path.setAttribute('d', resizedPath);
 const paragraphs = document.querySelectorAll('p')
 
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach((entry, index) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show')
-      entry.target.style.transform = "translateX(0)"
-    }
-  })
-})
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach((entry, index) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show')
+//       entry.target.style.transform = "translateX(0)"
+//     }
+//   })
+// })
 
-paragraphs.forEach(para => {
-  observer.observe(para)
-})
+// paragraphs.forEach(para => {
+//   observer.observe(para)
+// })
 
-spaceship.style.offsetPath =  `path("${path.getAttribute("d")}")`; 
+spaceship.style.offsetPath =  `path("${resizedPath}")`; 
 
-window.onscroll = () => {
-  const body = document.body;
-  const parent = document.body.parentNode;
-  const scrollAmount =
-    ((body.scrollTop || parent.scrollTop) /
-      (parent.scrollHeight - parent.clientHeight)) *
-    100;
+$(window).on('scroll', function(){
+  const s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
 
-  spaceship.style.setProperty("--offset-distance", scrollAmount + "%");
-};
+  // console.log(s);
+  const scrollPercent = (s / (d - c)) * 100;
+  
+  console.clear();
+  console.log(scrollPercent);
+
+  spaceship.style.setProperty("--offset-distance", scrollPercent + "%");
+});
