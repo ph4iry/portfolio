75ce85982e48e9f50980cf6cf1821703ae8a5d1f
Phaedra <71579580+ph4iry@users.noreply.github.com>
2023-05-11 01:32:25 -0400

    changed structure and now has tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["**/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['"Work Sans"', ...defaultTheme.fontFamily.sans],
+      'mono': ['"Space Mono"', ...defaultTheme.fontFamily.mono],
+      'display': ['"Cairo Play"'],
+    },
+//     keyframes: {
+//       'spin-right': {
+//         '100%': {
+//           '-webkit-transform': 'rotate(360deg)',
+//           '-moz-transform': 'rotate(360deg)',
+//           '-ms-transform': 'rotate(360deg)',
+//           '-o-transform': 'rotate(360deg)',
+//           transform: 'rotate(360deg)'
+//         }
+//       }
+
+//       /*
+//       @-webkit-keyframes spin-right {
+//   100% {
+//     '-webkit-transform': rotate(360deg),
+//     '-moz-transform': rotate(360deg),
+//     '-ms-transform': rotate(360deg),
+//     '-o-transform': rotate(360deg),
+//     transform: rotate(360deg);
+//   }
+// }
+
+//       */
+//     }
+  },
+  plugins: [],
+}
+
