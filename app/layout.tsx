d52164e3caf4406df9202631f3e455cb424d9949
Ctech 21L <71579580+ph4iry@users.noreply.github.com>
2023-07-12 17:11:37 -0400

    next-ified

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Raleway } from 'next/font/google';
+import { ReactNode } from 'react';
+
+const raleway = Raleway({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Welcome to the Phaedraverse',
+  description: 'Phaedra Sanon\'s personal website',
+  viewport: { width: 'device-width', initialScale: 1 },
+};
+
+/*
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to Phaedraverse</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" /> -->
+  <link href="./css/output.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="./css/orbit.css" rel="stylesheet">
+*/
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
+        {/* <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" /> --> */}
+        {/* <link href="./css/output.css" rel="stylesheet"/> */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        {/* <link href="./css/orbit.css" rel="stylesheet"/> */}
+      </head>
+      <body className={`${raleway.className} h-screen bg-[#22252C]`}>{children}</body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  content: ["**/*.html", "./node_modules/flowbite/**/*.js"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
-      colors: {
-        'muted': '#9DA3AE'
-      }
-    },
-    fontFamily: {
-      'sans': ['"Work Sans"', ...defaultTheme.fontFamily.sans],
-      'mono': ['"Space Mono"', ...defaultTheme.fontFamily.mono],
-      'display': ['"Cairo Play"'],
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
+  plugins: [],
 }
-
