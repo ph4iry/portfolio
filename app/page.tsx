bc17423f7d6a9f00700fe9058643a2be589e882c
Phaedra <psanon@bostonk12.org>
2024-01-18 17:54:40 -0500

    edited stuff

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      <ThreeScene />
+    <main className="flex items-center justify-center w-screen h-screen">
+      <div className="absolute top-0 left-0 h-screen w-screen z-[-9999]">
+        <ThreeScene />
+      </div>
+      <div className="md:mx-24 rounded bg-gray-100/50 w-full max-h-[83%] h-full">
+        <div className="w-full h-6">close exit</div>
+      </div>
     </main>
   )
 }
--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -1,46 +1,8 @@
 'use client';
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei';
-// import { Application } from '@splinetool/runtime';
-
-// const canvas = document.getElementById('canvas3d');
-// const app = new Application(canvas);
-// app.load('https://prod.spline.design/E4yVCOqZ3Z8TEY9f/scene.splinecode');
-
-
-function Box(props: any) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => ((ref.current as any).rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import Spline from '@splinetool/react-spline';
 
 export default function ThreeScene() {
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls />
-    </Canvas>
-  )
+    <Spline style={{height: "100vh"}} scene="https://draft.spline.design/3gGOLrZAG1HGxgYI/scene.splinecode" />
+  );
 }
