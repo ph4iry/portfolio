6d1d2b71a58dc79b08f7e004a2edaa653abba8bd
Phaedra <71579580+ph4iry@users.noreply.github.com>
2024-02-21 01:42:29 -0500

    added about me summary and keypress 3d animation

--- components/scenes/AboutMeScene.tsx
+++ components/scenes/AboutMeScene.tsx
@@ -1,7 +1,33 @@
-import Spline from '@splinetool/react-spline';
+'use client';
+import Spline, { SplineProps } from '@splinetool/react-spline';
+import { MutableRefObject, useEffect, useRef } from 'react';
 
-export default function AboutMeScene() {
+export default function AboutMeScene({ button }: { button: MutableRefObject<HTMLButtonElement> }) {
+  const spline = useRef<any>(null!);
+
+  const onLoad = (splineApp: any) => {
+    spline.current = splineApp;
+  }
+
+  const handleClick = () => {
+    // 9510fda7-f48c-4fb1-a76f-f58240e6c413
+    console.log('click');
+    spline.current!.emitEvent('keyDown', '9510fda7-f48c-4fb1-a76f-f58240e6c413');
+  }
+
+  useEffect(() => {
+    const curr = button.current;
+    curr.addEventListener('click', handleClick);
+
+    return () => {
+      curr.removeEventListener('click', handleClick);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <Spline scene="https://prod.spline.design/4KyWCqZwrg560IJG/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/4KyWCqZwrg560IJG/scene.splinecode"
+      onLoad={onLoad}
+      
+    />
   );
 }
