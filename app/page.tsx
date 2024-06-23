e92e1837f7f8473aa7b6911360821dba4c841c38
Phaedra <71579580+ph4iry@users.noreply.github.com>
2024-06-23 16:28:03 -0400

    attempted loader animation...
    does not work at the moment, will fix in the next hour

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import MapTransition from '@/components/MapTransition'
-
 export default function Home() {
   return (
+    // <div className="bg-white w-screen h-screen flex justify-center items-center">
+    //   <MapTransition />
+    // </div>
     <MapTransition />
   )
 }
--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,7 @@
 import { Transition } from '@headlessui/react'
 import { useProgress } from '@react-three/drei'
 import { useEffect } from 'react'
+import LoadingAnimation from './LoadingAnimation'
 
 export default function Loader({ started, onStarted }:{started: boolean, onStarted: () => void}) {
   const { progress } = useProgress()
@@ -25,22 +26,29 @@ export default function Loader({ started, onStarted }:{started: boolean, onStart
     <Transition
       show={!started}
       as='div'
-      leave="transition-all"
+      leave="transition"
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
     >
-      <div className={`absolute z-[9999] top-0 left-0 bg-white flex flex-col gap-4 justify-center items-center w-screen h-screen ${started ? 'hidden' : ''}`}>
-        <div className='text-2xl font-bold'>
+      <div className={`absolute z-[9999] top-0 left-0 bg-[#110326]/70 backdrop-blur-md flex flex-col gap-4 justify-center items-center w-screen h-screen text-white ${started ? 'hidden' : ''}`}>
+        <div className='text-2xl font-bold italic'>
           Loading the Phaedraverse...
         </div>
+        <LoadingAnimation />
         <div className='flex flex-col'>
-          <h1 className='loadingScreen__title'>Title</h1>
-          <button
-            className='disabled:bg-red-400/30 bg-emerald-400 px-4 py-2 rounded-full' disabled={progress < 100}
-            onClick={onStarted}
+          <Transition
+            show={progress === 100}
+            enter="transition"
+            enterFrom="opacity-0 translate-y-6"
+            enterTo="opacity-100 translate-y-0"
           >
-            Start
-          </button>
+            <button
+              className='disabled:bg-red-400/30 bg-emerald-400 px-4 py-2 rounded-full' disabled={progress < 100}
+              onClick={() => setTimeout(() => onStarted(), 0) }
+            >
+              Start
+            </button>
+          </Transition>
         </div>
       </div>
     </Transition>
